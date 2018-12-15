54c4091ad4589270ac8d48e5c146eb76f6daf720
Will Sabransky <will.sabransky@postmates.com>
2018-12-15 02:39:54 -0800

    maybe fix types?

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,16 @@ import ts from 'rollup-plugin-typescript2';
 export default [{
   input: 'src/index.tsx',
   output: [
-    { file: 'dist/index.cjs.js', format: 'cjs' },
+    { file: 'dist/index.js', format: 'cjs' },
     { file: 'dist/index.es.js', format: 'es' },
   ],
   external: [
     'react',
   ],
   plugins: [
-    ts(),
+    ts({
+      typescript: require('typescript'),
+      useTsconfigDeclarationDir: true,
+    }),
   ],
 }];
